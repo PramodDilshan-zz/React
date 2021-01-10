e504e599d24f9d87459c93bc0cbdffd19021fa21
Pramod Dilshan Rathnayake <pramod.dilshan.rathnayake@gmail.com>
2021-01-10 15:58:29 +0530

    rendering elemnts

---
+++ src/components/counter.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export default class counter extends Component {
+
+    state = {
+        count: 0,
+        tags: ['tags1', 'tags2', 'tags3']
+    };
+
+    render() {
+
+        return (
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increament</button>
+                <ul>
+                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+                </ul>
+            </div>
+        )
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count) === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount(){
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+}
