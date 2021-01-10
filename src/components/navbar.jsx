19de37b2400d498c1723e58e01c5bcd7d1d7e37a
Pramod Dilshan Rathnayake <pramod.dilshan.rathnayake@gmail.com>
2021-01-10 19:59:04 +0530

    functional component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
 
-export default class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-light bg-light">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              Navbar
-            </a>
-            <span className="badge badge-pill badge-secondary">
-              {this.props.totalCounters}
-            </span>
-          </div>
-        </nav>
-      </div>
-    );
-  }
-}
+const Navbar = (props) => {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <span className="badge badge-pill badge-secondary">
+            {props.totalCounters}
+          </span>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
